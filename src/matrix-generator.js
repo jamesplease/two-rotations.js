8d417af1f6473d5eb265fc5df789b6b6ee050671
Jmeas <jellyes2@gmail.com>
2015-03-22 16:23:23 -0400

    Fix comments in source.

--- src/matrix-generator.js
+++ src/matrix-generator.js
@@ -1,8 +1,8 @@
 // Generate a rotation matrix from a given yaw and pitch
 // This matrix is a combination of two rotations:
-// Rx * Rz
+// Ry * Rx
 // Where Ri is the 3d rotation matrix about that axis
-// We call the angle about the z-axis the 'yaw,'
+// We call the angle about the y-axis the 'yaw,'
 // and the angle about the x-axis is the 'pitch.'
 function matrixGenerator(yaw = 0, pitch = 0) {
   var cosP = Math.cos(pitch);
